8db717e45388d3c5dcdf47eed94913d1dcf84f54
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2017-04-06 18:22:00 +0700

    Add php variable config file

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <noscript><meta http-equiv="refresh" content="0; url=noscript.html" /></noscript>
+  <script>window.location.replace('login.php');</script>
+</head>
+<body>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<?php $config = include('data/config.php'); ?>
+<html>
+<head>
+  <noscript><meta http-equiv="refresh" content="0; url=noscript.html" /></noscript>
+  <title><?php echo "Login :: ".$config['project_name'];?></title>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <script>
+  </script>
+</head>
+<body>
+</body>
+</html>
